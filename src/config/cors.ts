cdedf930d136e98d2746344e4ea4abf9ad3cdae6
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-02-25 18:48:01 +0530

    Passing Single Origin to cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,18 +1,6 @@
 // Setting dynamic origins
-const corsOriginWhitelist = [process.env['CLIENT_BASE_URL']];
-
-type StaticOrigin = boolean | string | RegExp | (boolean | string | RegExp)[];
 const corsOptionForCredentials = {
-  origin: function (
-    origin: string | undefined,
-    callback: (err: Error | null, origin?: StaticOrigin | undefined) => void,
-  ) {
-    if (origin && corsOriginWhitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: process.env['CLIENT_BASE_URL'],
   methods: ['GET', 'POST', 'DELETE', 'PUT'],
   credentials: true,
 };
