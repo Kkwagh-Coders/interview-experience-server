97b5481ff5e36c1b52f00d798f78e412fbf2730e
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-09-26 21:46:09 +0530

    feat: Changed the AI modal of hugging face

--- src/utils/generateSummaryUsingAI.ts
+++ src/utils/generateSummaryUsingAI.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const generateSummaryUsingAI = async (content: string) => {
   const url =
-    'https://api-inference.huggingface.co/models/facebook/bart-large-cnn';
+    'https://api-inference.huggingface.co/models/tuner007/pegasus_summarizer';
 
   const data = { inputs: content };
   const response = await axios.post<[{ summary_text: string }]>(url, data, {
