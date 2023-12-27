5b11f294ed2786b7c6db55faaec2aa34c8eb4e8a
AlgorithmChopda <vinaychopda4@gmail.com>
2023-12-27 16:09:53 +0530

    feat: added response handlers

---
+++ src/utils/apiResponse.ts
@@ -0,0 +1,21 @@
+import { Response } from 'express';
+
+export const SuccessResponse = (res: Response, data: object) => {
+  return res.status(200).json(data);
+};
+
+export const BadRequestError = (res: Response, data: object) => {
+  return res.status(400).json(data);
+};
+
+export const UnauthorizedError = (res: Response, data: object) => {
+  return res.status(401).json(data);
+};
+
+export const NotFoundError = (res: Response, data: object) => {
+  return res.status(404).json(data);
+};
+
+export const InternalServerError = (res: Response, data: object) => {
+  return res.status(500).json(data);
+};
